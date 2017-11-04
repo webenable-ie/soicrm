a1ed0a64d007fce97984d43543dada45dcf38ecd
Tom <tom@webenable.ie>
2017-11-04 19:10:52 +0000

    04112017-3

--- clubs/urls.py
+++ clubs/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import ClubList
+from .views import ClubListView, ClubDetailsView
 
 urlpatterns=[
-    url(r'^$', ClubList.as_view(), name="clublist"),
+    url(r'^$', ClubListView.as_view(), name="club_list"),
+    url(r'^(?P<slug>[-\w]+)/$', ClubDetailsView.as_view(), name="club_details")
 ]
--- clubs/views.py
+++ clubs/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Club
 
 # Create your views here.
 
 
-class ClubList(ListView):
+class ClubListView(ListView):
     def get_queryset(self):
         return Club.objects.all()
+
+
+class ClubDetailsView(DetailView):
+
+    model = Club
+
+    def get_context_data(self, **kwargs):
+        context = super(ClubDetailsView, self).get_context_data(**kwargs)
+        return context
